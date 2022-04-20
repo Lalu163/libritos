a71f2dde84fca690b3f9ea5b93822dc8fc2f62c6
Laura Contreras <m.laura.cp89@gmail.com>
2022-04-20 21:07:24 +0200

    adding book repository

---
+++ src/main/java/laura/portfolio/libritos/controllers/BookController.java
@@ -0,0 +1,29 @@
+package laura.portfolio.libritos.controllers;
+
+import laura.portfolio.libritos.repositories.Book;
+import laura.portfolio.libritos.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @GetMapping("/books")
+    String listBooks(Model model){
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("title", "Book list");
+        model.addAttribute("books", books);
+        return "books/all";
+    }
+}
--- src/test/java/laura/portfolio/libritos/ApplicationTests.java
+++ src/test/java/laura/portfolio/libritos/ApplicationTests.java
@@ -1,14 +1,16 @@
 package laura.portfolio.libritos;
 
+import laura.portfolio.libritos.repositories.Book;
+import laura.portfolio.libritos.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -22,5 +24,18 @@ public class ApplicationTests {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("home"));
+    }
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void returnsTheExistingBooks() throws Exception {
+
+        Book book = bookRepository.save(new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", "fantasy"));
+
+        mockMvc.perform(get("/books"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("books/all"))
+                .andExpect(model().attribute("books", hasItem(book)));
     }
 }
